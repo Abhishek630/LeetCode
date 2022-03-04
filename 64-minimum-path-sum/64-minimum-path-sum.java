c757e3b3a7e7ea694f4ca9bbb9a05587cc8e8fa2
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-03-05 01:49:06 +0530

    Time: 3 ms (60.73%), Space: 45.6 MB (41.23%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,16 +1,21 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-      return backtrack(grid, 0 , 0);
+        int m = grid.length, n = grid[0].length;
+        if (m == 1 && n == 1) return grid[0][0];
+        
+        for(int i = 1; i < m; i++) {
+            grid[i][0] = grid[i-1][0] + grid[i][0];
+        }
+        for(int j = 1; j < n; j++) {
+            grid[0][j] = grid[0][j-1] + grid[0][j];
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                grid[i][j] = Math.min(grid[i-1][j], grid[i][j-1]) + grid[i][j];
+            }
+        }
+        return grid[m-1][n-1];
+ 
     }
-    
-     int backtrack(int[][] grid, int r, int c){
-        if(r>= grid.length || c>=grid[0].length) return Integer.MAX_VALUE;
-        if(r == grid.length - 1 && c == grid[0].length -1) return grid[r][c];
-        if(grid[r][c] < 0) return -1*grid[r][c];
-        int temp =  grid[r][c] + Math.min(backtrack(grid, r+1, c) ,  backtrack(grid, r, c+1));
-        grid[r][c] = -1*temp;
-        return temp;
-         
-     }
-    
 }
