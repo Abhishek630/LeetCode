af2ef7d2b8864c74e5e601713432ca9702d44775
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-03-04 11:38:07 +0530

    Time: 6 ms (77.17%), Space: 44.2 MB (53.43%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        
+        double glass[][] = new double[query_row+1][query_glass+2];
+        glass[0][0] = poured;
+        
+        for(int i=0; i<query_row;i++){
+            for(int j=0; j<=query_glass;j++){
+                if(glass[i][j]>1.0){
+                    double rem = glass[i][j]-1;
+                    rem /=2;
+                    glass[i][j]=1;
+                    glass[i+1][j] += rem;
+                    glass[i+1][j+1] += rem;
+                }
+            }
+        }
+        return Math.min(glass[query_row][query_glass],1.0);
+//          if(k == 0) return 0;
+//         double[][] dp = new double[r+1][c+2];
+//         dp[0][0] = k;//putting all the water in the top of the matrix
+        
+//         for(int i = 0 ; i < r ; i++){
+//             for(int j = 0 ; j <= c ;  j++){
+//                 if(dp[i][j] > 1.0){//then divide the spare one
+//                     double spare = dp[i][j] - 1;//as this one will be there only in the glass
+//                     spare /= 2;//sapre will be equally divided
+//                     dp[i][j] = 1;//holding accoring to its capacity
+//                     dp[i+1][j] += spare;//left side having the half of extra water + add extra water
+//                     dp[i+1][j+1] += spare;//right side with the half one
+//                 }
+//             }
+//         }
+//         return Math.min(dp[r][c],1.0); 
+        
+    }
+}
