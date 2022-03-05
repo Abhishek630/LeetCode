00556cbe15f0081eda841b1887b28ca22a46982e
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-03-05 06:02:43 +0530

    Time: 14 ms (92.88%), Space: 50.3 MB (57.85%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        int n = s.length();
+        char ch[] = s.toCharArray();
+        
+        int count =0;
+        
+        for(int i=0;i<n;i++){
+            if(ch[i]=='('){
+                count++;
+            }
+            if(ch[i]==')'){
+                if(count>0){
+                    count--;
+                }
+                else{
+                    ch[i]=0;
+                }
+            }
+        }
+        
+        count =0;
+        for(int i=n-1;i>=0;i--){
+            if(ch[i]==')'){
+                count++;
+            }
+            if(ch[i]=='('){
+                if(count>0){
+                    count--;
+                }
+                else{
+                    ch[i]=0;
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<n;i++){
+            if(ch[i]!=0){
+                sb.append(ch[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
