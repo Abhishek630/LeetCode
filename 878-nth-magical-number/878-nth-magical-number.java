43243440743e90c26ea039d5c9bc3249e5b59f3e
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-03-23 00:27:20 +0530

    Time: 1 ms (50.22%), Space: 40.9 MB (46.67%) - LeetHub

---
+++ 878-nth-magical-number/878-nth-magical-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int nthMagicalNumber(int n, int a, int b) {
+        long A = a, B = b;
+        int mod =  1_000_000_007;               
+        long l = Math.min(a,b), r = (long) n * Math.min(a, b);        				
+        while (B > 0) {
+            long t = A;
+            A = B;
+            B = t % B;
+        }
+        
+		long lcm = (a*b)/A;     		
+        while (l < r) {
+            long mid = l+(r-l)/2;
+            
+            // if number of magical number till mid is smaller than n ,
+            // move right of mid 
+            if ((mid / a) + (mid / b) - (mid / lcm) < n) l = mid + 1;   
+            else r = mid;
+        }
+        return (int)(l % mod);
+    }
+}
