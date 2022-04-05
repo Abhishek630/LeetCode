9a297770448af9cee9f8bbc847a96181f6de5f87
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-04-05 12:44:14 +0530

    Time: 6 ms (26.50%), Space: 74.3 MB (55.30%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -19,5 +19,28 @@ class Solution {
     }
 }
 /*
-
+int n = height.length;
+        int left =0;
+        int right = n-1;
+        int res=0;=
+        
+        while(left<right){
+            int width = right-left;
+            int h = Math.min(height[left], height[right]);
+            
+            int area = width*h;
+            res = Math.max(res, area);
+            
+            if(height[left]<height[right]){
+                left++;
+            }
+            else if(height[left]> height[left]){
+                right--;
+            }
+            else{
+                left++;
+                right--;
+            }
+        }
+        return res;
         */
