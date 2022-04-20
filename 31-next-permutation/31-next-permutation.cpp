b930d61cba3539ac5f2dfe4ea0c42f5dce69178c
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-04-20 13:50:24 +0530

    Time: 3 ms (86.84%), Space: 12.2 MB (37.80%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-//         int n = nums.size();
-//         int k,l;
+        int n = nums.size();
+        int k,l;
         
-//         for(k =n-2; k>=0; k--){
-//             if(nums[k] < nums[k+1]){
-//                 break;
-//             }
-//         }
-        
-//         if(k<0){
-//             reverse(nums.begin(), nums.end());
-//         }
-//         else{
-//             for(l =n-1 ; l> k ;l--){
-//                 if(nums[l]>nums[k]){
-//                     break;
-//                 }
-//             }
-//         }
-//         swap(nums[l], nums[k]);
-//         reverse(nums.begin()+k+1, nums.end());
-         int n=nums.size(), ind1, ind2;
-        
-        for(ind1=n-2; ind1>=0; ind1--)
-        {
-            if(nums[ind1]<nums[ind1+1]) break;
-        }
-        if(ind1<0)
-        {
-            sort(nums.begin(), nums.end());
-        }
-        else
-        {
-            for(ind2=n-1; ind2>ind1; ind2--)
-            {
-                if(nums[ind2]>nums[ind1]) break;
+        for(k =n-2; k>=0; k--){
+            if(nums[k] < nums[k+1]){
+                break;
             }
-            swap(nums[ind1], nums[ind2]);
-            reverse(nums.begin()+ind1+1, nums.end());
         }
+        
+        if(k<0){
+            reverse(nums.begin(), nums.end());
+        }
+        else{
+            for(l =n-1 ; l> k ;l--){
+                if(nums[l]>nums[k]){
+                    break;
+                }
+            }
+        swap(nums[l], nums[k]);
+        reverse(nums.begin()+k+1, nums.end());
+        }
+//          int n=nums.size(), ind1, ind2;
+        
+//         for(ind1=n-2; ind1>=0; ind1--)
+//         {
+//             if(nums[ind1]<nums[ind1+1]) break;
+//         }
+//         if(ind1<0)
+//         {
+//             sort(nums.begin(), nums.end());
+//         }
+//         else
+//         {
+//             for(ind2=n-1; ind2>ind1; ind2--)
+//             {
+//                 if(nums[ind2]>nums[ind1]) break;
+//             }
+//             swap(nums[ind1], nums[ind2]);
+//             reverse(nums.begin()+ind1+1, nums.end());
+//         }
     }
 };
