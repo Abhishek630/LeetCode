9b4b549ac13bcf4996c5dd6f57587f7d67080560
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-04-22 22:12:12 +0530

    Time: 162 ms (67.28%), Space: 208.1 MB (29.50%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,17 +1,21 @@
 class MyHashMap {
 public:
-     int arr[1000001];
+    int arr[1000001];
     MyHashMap() {
-        fill(arr, arr + 1000000, -1); // initalizing with -1.
+        fill(arr, arr+1000000, -1);
     }
-    void put(int key, int val) {
-        arr[key] = val;
+    
+    void put(int key, int value) {
+        arr[key]= value;
+        
     }
+    
     int get(int key) {
         return arr[key];
     }
+    
     void remove(int key) {
-        arr[key] = -1;
+        arr[key]=-1;
     }
 };
 
