f89796bba5b577c84a5efbbd9394ba1bf37e112e
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-02 02:06:20 +0530

    Time: 1 ms (87.48%), Space: 42.3 MB (38.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,44 +1,50 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         
-        Stack<Character>sutil = new Stack<>();
-        Stack<Character>tutil = new Stack<>();
-        for(int i=0; i<s.length(); i++){
-            if(sutil.size()==0 && s.charAt(i)=='#'){
-                continue;
-            }
-            if(s.charAt(i)=='#'){
-                sutil.pop();
-            }
-            else{
-                sutil.push(s.charAt(i));
-            }
-        }
+        int i = s.length()-1;
+        int j = t.length()-1;
+        int sSkip =0;
+        int tSkip =0;
         
-        for(int i=0; i<t.length(); i++){
-            if(tutil.size()==0 && t.charAt(i)=='#'){
-                continue;
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    sSkip++;
+                    i--;
+                }
+                else if(sSkip>0){
+                    sSkip--;
+                    i--;
+                }
+                else{
+                    break;
+                }
             }
-            if(t.charAt(i)=='#'){
-                tutil.pop();
+            
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    tSkip++;
+                    j--;
+                }
+                else if(tSkip>0){
+                    tSkip--;
+                    j--;
+                }
+                else{
+                    break;
+                }
             }
-            else{
-                tutil.push(t.charAt(i));
-            }
-        }
-        
-        if(sutil.size() != tutil.size()){
-            return false;
-        }
-        
-        for(int i=sutil.size()-1;i>=0;i--){
-            if(sutil.peek() != tutil.peek()){
+            
+            if(i>=0 && j>=0 && s.charAt(i)!= t.charAt(j)){
                 return false;
             }
-            else{
-                sutil.pop();
-                tutil.pop();
+            
+            if((i>=0)!= (j>=0)){
+                return false;
             }
+            i--;
+            j--;
+            
         }
         return true;
     }
