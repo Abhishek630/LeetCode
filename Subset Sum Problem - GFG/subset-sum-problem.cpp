069e690027f5c80d88456f0ee11a49d07c0ebea9
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-05 01:14:48 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -12,38 +12,36 @@ public:
     bool isSubsetSum(vector<int>arr, int sum){
         
     int n = arr.size();    
-    bool subset[n + 1][sum + 1];
- 
-    // If sum is 0, then answer is true
-    for (int i = 0; i <= n; i++)
-        subset[i][0] = true;
+    bool t[n+1][sum+1];
+        // for(int i=0; i<n+1; i++){
+        //     for(int j=0; j<sum+1; j++){
+        //         if(i==0){
+        //             t[i][j]= false;
+        //         }
+        //         if(j==0){
+        //             t[i][j] = true;
+        //         }
+        //     }
+        // }
+        for (int i = 0; i <= n; i++)
+        t[i][0] = true;
  
     // If sum is not 0 and set is empty,
     // then answer is false
     for (int i = 1; i <= sum; i++)
-        subset[0][i] = false;
- 
-    // Fill the subset table in bottom up manner
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (j < arr[i - 1])
-                subset[i][j] = subset[i - 1][j];
-            if (j >= arr[i - 1])
-                subset[i][j] = subset[i - 1][j]
-                               || subset[i - 1][j - arr[i - 1]];
+        t[0][i] = false;
+        
+        for(int i=1; i<n+1;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    t[i][j]= t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+                else if(arr[i-1]>j){
+                    t[i][j] = t[i-1][j];
+                }
+            }
         }
-    }
- 
-    /*   // uncomment this code to print table
-     for (int i = 0; i <= n; i++)
-     {
-       for (int j = 0; j <= sum; j++)
-          printf ("%4d", subset[i][j]);
-       cout <<"\n";
-     }*/
- 
-    return subset[n][sum];
-        // code here 
+        return t[n][sum];
     }
 };
 
