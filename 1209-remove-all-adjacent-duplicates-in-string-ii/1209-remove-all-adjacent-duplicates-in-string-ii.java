216391e799bad1111211abeb589c112c492ccbe8
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-07 00:18:34 +0530

    Time: 89 ms (29.85%), Space: 50 MB (50.46%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,61 +1,71 @@
-// class Solution {
-//     public String removeDuplicates(String s, int k) {
-//         Stack<Pair<Character, Integer>>st = new Stack<>();
-        
-//         for(char c : s.toCharArray()){
-//             if(st.isEmpty() || st.peek().getValue() !=c){
-//                 st.push(new Pair<>(c,1));
-//             }
-//             else{
-//                 Pair<Character, Integer> head = st.pop();
-//                 st.push(new Pair<>(head.getKey(), head.getValue()+1));
-//                 if(st.peek().getValue() == k){
-//                     st.pop();
-//                 }
-//             }
-//         }
-        
-//         String ans ="";
-//         while(!st.isEmpty()){
-//             Pair<Character, Integer> head = st.pop();
-//             int freq = head.getValue();
-//             while(freq>0){
-//                 ans = head.getKey()+ ans;
-//             }
-//         }
-//         return ans;
-//     }
-// }
-class Item {
+class P {
   char c;
   int freq;
 
-  public Item(char c, int freq) {
+  public P(char c, int freq) {
     this.c = c;
     this.freq = freq;
   }
 }
 
+
 class Solution {
-  public String removeDuplicates(String s, int k) {
-    StringBuilder sb = new StringBuilder();
-    LinkedList<Item> stack = new LinkedList<>();
-
-    for (final char c : s.toCharArray()) {
-      if (!stack.isEmpty() && stack.peek().c == c)
-        ++stack.peek().freq;
-      else
-        stack.push(new Item(c, 1));
-      if (stack.peek().freq == k)
-        stack.pop();
+    public String removeDuplicates(String s, int k) {
+        Stack<P>st = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(st.isEmpty() || st.peek().c !=c){
+                st.push(new P(c,1));
+            }
+            else{
+                st.peek().freq++;
+               }
+            if(st.peek().freq == k){
+                    st.pop();
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()){
+           P p = st.pop();
+             for (int i = 0; i < p.freq; ++i)
+             sb.append(p.c);
+        }
+        return sb.reverse().toString();
     }
-
-    while (!stack.isEmpty()) {
-      Item item = stack.pop();
-      for (int i = 0; i < item.freq; ++i)
-        sb.append(item.c);
-    }
-
-    return sb.reverse().toString();
-  }
 }
+
+
+// class Item {
+//   char c;
+//   int freq;
+
+//   public Item(char c, int freq) {
+//     this.c = c;
+//     this.freq = freq;
+//   }
+// }
+
+// class Solution {
+//   public String removeDuplicates(String s, int k) {
+//     StringBuilder sb = new StringBuilder();
+//     LinkedList<Item> stack = new LinkedList<>();
+
+//     for (final char c : s.toCharArray()) {
+//       if (!stack.isEmpty() && stack.peek().c == c)
+//         ++stack.peek().freq;
+//       else
+//         stack.push(new Item(c, 1));
+//       if (stack.peek().freq == k)
+//         stack.pop();
+//     }
+
+//     while (!stack.isEmpty()) {
+//       Item item = stack.pop();
+//       for (int i = 0; i < item.freq; ++i)
+//         sb.append(item.c);
+//     }
+
+//     return sb.reverse().toString();
+//   }
+// }
