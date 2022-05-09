b048b8ac8ced9be7367f0890ab8357da0d6a9e00
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-10 03:23:20 +0530

    Time: 40 ms (72.54%), Space: 25.1 MB (18.23%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,52 +1,52 @@
-// class Solution {
-// public:
-//     vector<int> nextGreaterElements(vector<int>& nums) {
-//         vector<int>v;
-//         stack<int>st;
-//         int n = nums.size();
-        
-//         for(int i=0;i<n-1;i++){
-//             st.push(nums[i]);
-//         }
-        
-//         for(int i=n-1; i>=0; i--){
-          
-//             while(!st.empty() && nums[i]>=st.top()){
-//                 st.pop();
-//             }
-//             if(st.empty()){
-//                 v.push_back(-1);
-//             }
-//             else{
-//                 v.push_back(st.top());
-//             }
-//             st.push(nums[i]);
-//         }
-        
-//         reverse(v.begin(), v.end());
-//         return v;
-//     }
-// };
-
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> v;
-        stack<int> s;
+        vector<int>v;
+        stack<int>st;
+        int n = nums.size();
         
-        for(int i=nums.size() - 1; i>=0; i--) s.push(nums[i]);
+        for(int i=n-1;i>=0;i--){
+            st.push(nums[i]);
+        }
         
-        for(int i=nums.size() - 1; i>=0; i--) {
-            while(!s.empty() && nums[i] >= s.top()) s.pop();
-                
-            if(s.empty()) v.push_back(-1);
-            else v.push_back(s.top());
-            
-            s.push(nums[i]);
+        for(int i=n-1; i>=0; i--){
+          
+            while(!st.empty() && nums[i]>=st.top()){
+                st.pop();
+            }
+            if(st.empty()){
+                v.push_back(-1);
+            }
+            else{
+                v.push_back(st.top());
+            }
+            st.push(nums[i]);
         }
         
         reverse(v.begin(), v.end());
-        
         return v;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         vector<int> v;
+//         stack<int> s;
+        
+//         for(int i=nums.size() - 1; i>=0; i--) s.push(nums[i]);
+        
+//         for(int i=nums.size() - 1; i>=0; i--) {
+//             while(!s.empty() && nums[i] >= s.top()) s.pop();
+                
+//             if(s.empty()) v.push_back(-1);
+//             else v.push_back(s.top());
+            
+//             s.push(nums[i]);
+//         }
+        
+//         reverse(v.begin(), v.end());
+        
+//         return v;
+//     }
+// };
