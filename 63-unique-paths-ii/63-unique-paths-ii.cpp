65a1fd9a84fac2f136746923e2b1b57de143061e
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-21 02:43:38 +0530

    Time: 4 ms (62.05%), Space: 7.8 MB (41.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,67 +1,28 @@
-// class Solution {
-// public:
-//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-//         int m = obstacleGrid.size();
-//         int n = obstacleGrid[0].size();
-        
-//         vector<vector<int>>dp(m, vector<int>(n, -1));
-//         return dfs(obstacleGrid, 0,0, m,n, dp);
-//     }
-    
-//     int dfs(vector<vector<int>>& obstacleGrid,int i, int j, int m , int n, vector<vector<int>>&dp){
-//         if(i>=m || j>=n|| obstacleGrid[i][j]==1){
-//             return 0;
-//         }
-        
-//         if(i== m-1 && j == n-1){
-//             return 1;
-//         }
-        
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         else{
-//             return dfs(obstacleGrid, i+1,j, m,n,dp)+dfs(obstacleGrid,i,j+1,m,n,dp);
-//         }
-        
-//     }
-// };
 class Solution {
 public:
-    int dfs(vector<vector<int>>& obstacleGrid,int i,int j,vector<vector<int>> &dp,int m ,int n)
-    {
-        // if current cell value is not inside the boundary then we can not proceed further return 0
-        if(i<0 || j<0 || i>=m || j>=n || obstacleGrid[i][j]==1)
-            return 0;
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
         
-        // if we have already computed the value then no need to calvulate it again
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return dfs(obstacleGrid, 0,0, m,n, dp);
+    }
+    
+    int dfs(vector<vector<int>>& obstacleGrid,int i, int j, int m , int n, vector<vector<int>>&dp){
+        if(i>=m || j>=n|| obstacleGrid[i][j]==1){
+            return 0;
         }
         
-        // if we have on last cell then tsake it as a path and return 1
-        if(i==m-1 && j==n-1)
-        {    
+        if(i== m-1 && j == n-1){
             return 1;
         }
         
-        // now call the same function for right move and down move and add that
-        // first this function will go for the right move and calculate it paths
-        // then after backtrack it will go to down and calculate their paths
-        // and at the end add up both the paths and return
-        //                      (right move)             (down move)
-        return dp[i][j]=dfs(obstacleGrid,i,j+1,dp,m,n)+dfs(obstacleGrid,i+1,j,dp,m,n);
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
-    {
-        int m=obstacleGrid.size();      // no. of rows
-        int n=obstacleGrid[0].size();   // no. of columns
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        else{
+            return dp[i][j]=dfs(obstacleGrid, i+1,j, m,n,dp)+dfs(obstacleGrid,i,j+1,m,n,dp);
+        }
         
-        // set all values as -1 for dp
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        
-        // return the possible paths
-        return dfs(obstacleGrid,0,0,dp,m,n);   
     }
 };
