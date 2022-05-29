f0525e5f611c59a5d3570d90c9d3769a39e346b2
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-30 03:58:19 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (7.28%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -4,9 +4,16 @@ class Solution {
         int xor =x^y;
         int count =0;
         
-        while(xor!=0){
-            xor=xor&(xor-1);
-            count++;
+//         while(xor!=0){
+//             xor=xor&(xor-1);
+//             count++;
+//         }
+//         return count;
+        
+        for(int i=0;i<32;i++){
+            if(((xor>>i)&1) ==1){
+                count++;
+            }
         }
         return count;
     }
