847a5e6e54ff560550ba0247c7d13a448bedbfc0
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-31 03:46:19 +0530

    Time: 1020 ms (36.90%), Space: 197.1 MB (20.88%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,37 +1,59 @@
 class Solution {
 public:
-	vector<vector<int>> ans;
-	vector<vector<int>> graph;
-
-	vector<int> firstTime, minTime, visited;
-	int time = 1;
-
-	void dfs(int node, int parent = -1) {
-		firstTime[node] = minTime[node] = time++;
-		visited[node] = true;
-
-		for(auto& child : graph[node]) {
-			if(child == parent)
-				continue;
-
-			if(!visited[child]) 
-				dfs(child, node);
-
-			minTime[node] = min(minTime[child], minTime[node]);
-
-			if(firstTime[node] < minTime[child])
-				ans.push_back({node, child});
-		}
-	}
-
-	vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-		firstTime.resize(n), minTime.resize(n), visited.resize(n), graph.resize(n);
-		for(auto& edge : connections) {
-			int nodeA = edge[0], nodeB = edge[1];
-			graph[nodeA].push_back(nodeB);
-			graph[nodeB].push_back(nodeA);
-		}
-		dfs(0);
-		return ans;
-	}
+    
+    void dfs(int node, int parent, int timer, vector<int>&disc,vector<int> &low,                        vector<bool>&vis,vector<vector<int>>&result,vector<vector<int>>&adj){
+        
+        vis[node]=true;
+        disc[node]=low[node]= timer++;
+        
+        for(auto nbr: adj[node]){
+            if(nbr== parent){
+                continue;
+            }
+            if(!vis[nbr]){
+                dfs(nbr,node,timer,disc,low,vis,result,adj);
+                low[node]= min(low[node], low[nbr]);
+                
+                if(low[nbr]>disc[node]){
+                    vector<int>ans;
+                    ans.push_back(nbr);
+                    ans.push_back(node);
+                    result.push_back(ans);
+                }
+            }
+            else{
+                low[node] = min(low[node],disc[nbr]);
+            }
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        
+        vector<vector<int>>adj(n);
+        // for(int i=0; i<connections.size();i++){
+        //     int u = connections[i][0];
+        //     int v =connections[1][1];
+        //     adj[u].push_back(v);
+        //     adj[v].push_back(u);
+        // }
+        for(auto it : connections){
+            adj[it[1]].push_back(it[0]);
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        int timer =0;
+        vector<int>disc(n,-1);
+        vector<int>low(n,-1);
+        int parent =-1;
+        vector<bool>vis(n,false);
+        
+        //dfs
+        
+        vector<vector<int>>result;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,parent,timer,disc,low,vis,result,adj);
+            }
+        }
+        return result;
+    }
 };
