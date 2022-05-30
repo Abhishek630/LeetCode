65327f11c4dfb6e482d84a11c040dd54b3e6c2b5
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-05-31 03:47:11 +0530

    Time: 1167 ms (21.26%), Space: 183.5 MB (39.00%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -29,16 +29,16 @@ public:
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         
         vector<vector<int>>adj(n);
-        // for(int i=0; i<connections.size();i++){
-        //     int u = connections[i][0];
-        //     int v =connections[1][1];
-        //     adj[u].push_back(v);
-        //     adj[v].push_back(u);
-        // }
-        for(auto it : connections){
-            adj[it[1]].push_back(it[0]);
-            adj[it[0]].push_back(it[1]);
+        for(int i=0; i<connections.size();i++){
+            int u = connections[i][0];
+            int v =connections[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
+        // for(auto it : connections){
+        //     adj[it[1]].push_back(it[0]);
+        //     adj[it[0]].push_back(it[1]);
+        // }
         
         int timer =0;
         vector<int>disc(n,-1);
