362c196eb293dacffc76446cebe0097490d6c2af
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-06-16 15:50:20 +0530

    Time: 215 ms (90.50%), Space: 16 MB (77.55%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,63 +1,63 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int m= nums1.size();
-        int n= nums2.size();
-        int dp[m+1][n+1];
+        // int m= nums1.size();
+//         int n= nums2.size();
+//         int dp[m+1][n+1];
         
-        // initialization
-        for(int i=0;i<=m;i++)
-            dp[i][0]=0;   // Eg LCS of "abc" & "" = 0
-        for(int j=0;j<=n;j++)
-            dp[0][j]=0;   // Eg LCS of "" & "abc" = 0
+//         // initialization
+//         for(int i=0;i<=m;i++)
+//             dp[i][0]=0;   // Eg LCS of "abc" & "" = 0
+//         for(int j=0;j<=n;j++)
+//             dp[0][j]=0;   // Eg LCS of "" & "abc" = 0
         
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(nums1[i-1]==nums2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=0;                   // ONLY THIS CHANGE
-            }
-        }
-    	 int maxLen=0;              // Now finding the max element
-         for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-               maxLen=max(maxLen,dp[i][j]);
-        }
-        return maxLen;
+//         for(int i=1;i<=m;i++)
+//         {
+//             for(int j=1;j<=n;j++)
+//             {
+//                 if(nums1[i-1]==nums2[j-1])
+//                     dp[i][j]=1+dp[i-1][j-1];
+//                 else dp[i][j]=0;                   // ONLY THIS CHANGE
+//             }
+//         }
+//     	 int maxLen=0;              // Now finding the max element
+//          for(int i=1;i<=m;i++)
+//         {
+//             for(int j=1;j<=n;j++)
+//                maxLen=max(maxLen,dp[i][j]);
+//         }
+//         return maxLen;
        
         
-//         int n = nums1.size();
-//         int m = nums2.size();
-//          vector<vector<int>>dp(102,vector<int>(102,-1));
+        int n = nums1.size();
+        int m = nums2.size();
+        int dp[n+1][m+1];
         
-//         for(int i=0; i<=n;i++){
-//             dp[i][0]=0;
-//         }
+        for(int i=0; i<=n;i++){
+            dp[i][0]=0;
+        }
         
-//          for(int i=0; i<=m;i++){
-//             dp[0][i]=0;
-//         }
+         for(int i=0; i<=m;i++){
+            dp[0][i]=0;
+        }
         
-//         for(int i=1;i<=n;i++){
-//             for(int j=1;j<=m;j++){
-//                 if(nums1[i-1]== nums2[j-1]){
-//                     dp[i][j] =1+dp[i-1][j-1];
-//                 }
-//                 else{
-//                     dp[i][j]=0;
-//                 }
-//             }
-//         }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]== nums2[j-1]){
+                    dp[i][j] =1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
         
-//         int count =0;
-//           for(int i=1;i<=n;i++){
-//             for(int j=1;j<=m;j++){
-//                count = max(count,dp[i][j]);
-//             }
-//         }
-//         return count;
+        int count =0;
+          for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+               count = max(count,dp[i][j]);
+            }
+        }
+        return count;
     }
 };
