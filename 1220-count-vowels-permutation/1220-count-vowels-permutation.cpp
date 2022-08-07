0cb3e8535bf126e01df08ee3dccfcb278f13275f
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-08-08 02:13:27 +0530

    Time: 374 ms (12.61%), Space: 67 MB (11.37%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,42 +3,50 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        if(n==1) return 5;
-        unordered_map<char,ll> m1;
-        unordered_map<char,ll> m2;
-        // we will in map 1 that the number of occurences of a vowel
-        m1['a']=1; m1['e']=1; m1['i']=1; m1['o']=1; m1['u']=1;
-        
-        for(int i=1;i<n;i++){
-            for(auto c: m1){  // we will interate map 1 and we will add the number of appearances
-                if(c.first=='a'){ // of a letter to the letter followed by it in map2
-                    m2['e'] += (c.second%mod);
-                }else if(c.first=='e'){
-                    m2['a'] += (c.second%mod);
-                    m2['i'] += (c.second%mod);
-                }else if(c.first=='i'){
-                    m2['a'] += (c.second%mod);
-                    m2['e'] += (c.second%mod);
-                    m2['o'] += (c.second%mod);
-                    m2['u'] += (c.second%mod);
-                }else if(c.first=='o'){
-                    m2['i'] += (c.second%mod);
-                    m2['u'] += (c.second%mod);
-                }else if(c.first=='u'){
-                    m2['a'] += (c.second%mod);
-                }
-            }
-            // then we will equate map1 with map2 for next iteration and clear map2 for next iteration
-            m1 = m2;
-            m2.clear();
+        if(n==1){
+            return 5;
         }
-        ll ans=0;
-		// in the final step we will add the number of occurences of a vowel at the last index
-        for(auto c: m1){
-            ans += (c.second%mod);
+        unordered_map<char,ll>mp1;
+        unordered_map<char,ll>mp2;
+        mp1['a'] =1;
+        mp1['e'] =1;
+        mp1['i'] =1;
+        mp1['o'] =1;
+        mp1['u'] =1; 
+    
+    for(int i=1; i<n; i++){
+        for(auto c:mp1){
+            if(c.first =='a'){
+                mp2['e'] += (c.second%mod);
+            }
+            if(c.first =='e'){
+                mp2['a'] += (c.second%mod);
+                mp2['i'] += (c.second%mod);
+            }
+            if(c.first =='i'){
+                mp2['a'] += (c.second%mod);
+                mp2['e'] += (c.second%mod);
+                mp2['o'] += (c.second%mod);
+                mp2['u'] += (c.second%mod);
+            }
+            if(c.first =='o'){
+                mp2['i'] += (c.second%mod);
+                mp2['u'] += (c.second%mod);
+            }
+            if(c.first =='u'){
+                mp2['a'] += (c.second%mod);
+            }
+        }
+        
+        mp1=mp2;
+        mp2.clear();
+    }
+        long long ans =0;
+        
+        for(auto c : mp1){
+            ans = ans+(c.second%mod);
             ans%=mod;
         }
-        
         return ans;
     }
 };
