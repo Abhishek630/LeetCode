78598f31ce55a1923919c3eff74560e65d1ee1b1
Abhishek630 <46994856+Abhishek630@users.noreply.github.com>
2022-08-22 23:21:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-         return n > 0 ? pow(4, int(log(n) / log(4))) == n : false;
+         // return n > 0 ? pow(4, int(log(n) / log(4))) == n : false;
+        if(n>0){
+            if(pow(4,int(log(n)/log(4)))==n){
+                return true;
+            }
+        }
+        return false;
     }
 };
